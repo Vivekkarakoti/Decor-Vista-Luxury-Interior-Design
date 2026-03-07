c1572dca306e0e2303ce210fe8b6cf46fc0741f3
Vivekkarakoti <Viveksngh343@gmail.com>
2026-03-08 01:55:16 +0530

    Initial upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "/DecorVista/",
+  base: "/Decor-Vista-Luxury-Interior-Design/",
 
   plugins: [react(), tailwindcss()],
 
